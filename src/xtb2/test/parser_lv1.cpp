e06df3f329669b27e1e59247c48b79cad46c30ef
fgalliat <fgalliat@gmail.com>
2021-08-09 22:51:53 +0200

    feat(xtb): add Level1 Parser (wip)

--- src/xtb2/test/parser_lv1.cpp
+++ src/xtb2/test/parser_lv1.cpp
@@ -8,6 +8,140 @@
 void br() { printf("\n"); }
 void disp(const char* str) { printf("%s\n", str); }
 
+bool startsWith(char* str1, const char* strToFind) {
+    int tlen = strlen( strToFind );
+    if ( strlen(str1) < tlen ) { return false; }
+    return strncmp(str1, strToFind, tlen) == 0;
+}
+
+bool contains(char* str1, const char* strToFind) {
+    int tlen = strlen( strToFind );
+    if ( strlen(str1) < tlen ) { return false; }
+    return strstr(str1, strToFind) != NULL;
+}
+
+int indexOf(char* str, char ch, int start=0) {
+  if ( start < 0 ) {
+      start = 0;
+  }
+
+  int len = strlen(str);
+  if ( start >= len ) {
+    //   start = -1;
+    return -1;
+  }
+
+  for(int i=start; i < len; i++) {
+      if ( str[i] == ch ) {
+          return i;
+      }
+  }
+
+return -1;
+}
+
+// always free-able
+char* makeString(int len) {
+    char* p = (char*)malloc(len+1);
+    p[len] = (char)0x00;
+  return p;
+}
+
+// makes a safe copy
+char* substring(char* str, int start, int stop) {
+    int len = strlen(str);
+    if ( start >= len ) {
+        return makeString(0);
+    }
+    if ( stop < 0 ) {
+        stop = len;
+    }
+    if ( start < 0 ) {
+        start = 0;
+    }
+    int rlen = stop-start;
+    char* res = makeString( rlen );
+
+    for(int i=0; i < rlen; i++) {
+        res[i] = str[i+start];
+    }
+
+    return res;
+}
+
+// modify str
+char* trim(char* str) {
+  if ( str == NULL || strlen(str) < 1 ) { return str; }
+  int tlen = strlen(str);
+  int left = 0;
+  int right = tlen-1;
+  for(int i=0; i < tlen; i++) {
+      if ( str[i] > 32 ) {
+          left = i;
+          break;
+      }
+  }
+  for(int i=tlen-1; i >= 0; i--) {
+      if ( str[i] > 32 ) {
+          right = i;
+          break;
+      }
+  }
+
+  if ( left > 0 || right < tlen-1 ) {
+      if (left > 0      ) memmove(&str[0], &str[left], tlen-left);
+      if (right < tlen-1) str[right+1] = 0x00;
+  }
+
+  return str;
+}
+
+
+// ========================
+char* varsNames[99];
+addr varsAddrs[99];
+int varMax = 0;
+
+void addNewVar(dataType type, char* name, int len=-1) {
+    printf("Add var : (%s)\n", name);
+  varsNames[varMax] = name;
+  varsAddrs[varMax] = addData(type, 1, len);
+  varMax++;
+}
+
+int findVar(char* name) {
+    for(int i=0; i < varMax; i++) {
+        int tlen = strlen(name);
+        if ( strlen(varsNames[i]) == tlen && strncmp(varsNames[i], name, tlen) == 0 ) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+char* lblsNames[99];
+addr lblsAddrs[99];
+int lblMax = 0;
+
+void addLabel(char* name) {
+    printf("Add lbl : (%s)\n", name);
+  lblsNames[lblMax] = name;
+  lblsAddrs[lblMax] = curCodePosition; // from xcomp.h
+  lblMax++;
+}
+
+int findLabel(char* name) {
+    for(int i=0; i < lblMax; i++) {
+        int tlen = strlen(name);
+        if ( strlen(lblsNames[i]) == tlen && strncmp(lblsNames[i], name, tlen) == 0 ) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// ========================
+
 void handleLine(char* line) {
     int tlen = strlen(line);
     if ( tlen == 0 ) { return; }
@@ -22,6 +156,80 @@ void handleLine(char* line) {
     tlen = strlen(line);
     if ( tlen == 0 ) { return; }
 
+    if ( startsWith(line, "//") ) {
+        return;
+    } 
+    // ======= Var Def ===============
+    else if ( startsWith(line, "byte ") ) {
+        char* descr = &line[5];
+        int len = strlen(descr);
+        if ( contains( descr, "=" ) ) {
+            len = indexOf(descr, '=')+1;
+        }
+
+        char* varName = trim( substring( descr, 0, len-1 ) );
+        addNewVar(T_BYTE, varName);
+
+        // FIXME .. freemem
+    } else if ( startsWith(line, "float ") ) {
+        char* descr = &line[6];
+        int len = strlen(descr);
+        if ( contains( descr, "=" ) ) {
+            len = indexOf(descr, '=')+1;
+        }
+
+        char* varName = trim( substring( descr, 0, len-1 ) );
+        addNewVar(T_FLOAT, varName);
+
+        // FIXME .. freemem
+    } else if ( startsWith(line, "string[") ) {
+        char* descr = &line[7];
+        int dMax = indexOf(descr, ']')+1;
+        char* dStr = trim( substring( descr, 0, dMax-1 ) );
+        int ln = atoi(dStr);
+        descr = &descr[dMax];
+
+        int len = strlen(descr);
+        if ( contains( descr, "=" ) ) {
+            len = indexOf(descr, '=')+1;
+        }
+
+        char* varName = trim( substring( descr, 0, len-1 ) );
+
+        addNewVar(T_STRING, varName, ln);
+        // FIXME ..
+    }
+    // condition
+    else if ( startsWith(line, "if ") ) {
+        if ( contains(line, "==") ) {
+            
+        } else if ( contains(line, "!=") ) {
+            
+        } else if ( contains(line, ">=") ) {
+            
+        } else if ( contains(line, "<=") ) {
+            
+        } else if ( contains(line, ">") ) {
+            
+        } else if ( contains(line, "<") ) {
+            
+        }
+    }
+    // assignation
+    else if ( contains(line, "=") ) { // FIXME : contained in str ..
+    }
+    else if ( startsWith(line, "inc ") ) {
+    }
+    // calls
+    else if ( startsWith(line, "disp ") ) {
+    }
+    // labels
+    else if ( startsWith(line, "lbl ") ) {
+        char* lblName = &line[4];
+        addLabel(lblName);
+    }
+
+    else
     printf("> %s\n", line);
 }
 
